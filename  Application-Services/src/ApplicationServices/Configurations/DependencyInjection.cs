9070773deca833d8a32d819df2bda075606f4372
prashantpic <prashantpic001@gmail.com>
2025-05-15 12:22:32 +0000

    Files added by TheSSS-AI

---  Application-Services/src/ApplicationServices/Configurations/DependencyInjection.cs
+++  Application-Services/src/ApplicationServices/Configurations/DependencyInjection.cs
@@ -1,59 +1,27 @@
 using System.Reflection;
 using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using AutoMapper;
+using TheSSS.DicomViewer.Application.Interfaces.Application;
+using TheSSS.DicomViewer.Application.Services;
 
 namespace TheSSS.DicomViewer.Application.Configurations;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddMediatR(cfg => 
             cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-        
-        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-        services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
-        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         services.AddScoped<IDicomImportService, DicomImportOrchestratorService>();
         services.AddScoped<IAnonymizationOrchestrationService, AnonymizationOrchestrationService>();
         services.AddScoped<IDicomNetworkService, DicomNetworkOrchestratorService>();
 
         return services;
-    }
-
-    private sealed class RequestValidationBehavior<TRequest, TResponse> 
-        : IPipelineBehavior<TRequest, TResponse>
-        where TRequest : IRequest<TResponse>
-    {
-        private readonly IEnumerable<IValidator<TRequest>> _validators;
-
-        public RequestValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
-        {
-            _validators = validators;
-        }
-
-        public async Task<TResponse> Handle(
-            TRequest request, 
-            RequestHandlerDelegate<TResponse> next, 
-            CancellationToken cancellationToken)
-        {
-            var context = new ValidationContext<TRequest>(request);
-            var failures = _validators
-                .Select(v => v.Validate(context))
-                .SelectMany(result => result.Errors)
-                .Where(f => f != null)
-                .ToList();
-
-            if (failures.Count != 0)
-            {
-                throw new ValidationException(failures);
-            }
-
-            return await next();
-        }
     }
 }
