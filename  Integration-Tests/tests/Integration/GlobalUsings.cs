c6cc60c0e2212d2aa79f228611dbe17a0af1cca7
prashantpic <prashantpic001@gmail.com>
2025-05-15 13:21:01 +0000

    Files added by TheSSS-AI

---  Integration-Tests/tests/Integration/GlobalUsings.cs
+++  Integration-Tests/tests/Integration/GlobalUsings.cs
@@ -1,27 +1,22 @@
 global using Xunit;
 global using FluentAssertions;
 global using Moq;
-
+global using System.Threading.Tasks;
+global using System.Collections.Generic;
+global using System.Linq;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.Logging;
-
-global using System;
-global using System.Collections.Generic;
 global using System.IO;
-global using System.Linq;
-global using System.Threading;
-global using System.Threading.Tasks;
+global using System.Diagnostics;
 
-global using TheSSS.DicomViewer.Application;
-global using TheSSS.DicomViewer.Application.Services; // Assuming service interfaces are here
-global using TheSSS.DicomViewer.Domain.Entities; // Assuming domain entities are here
-global using TheSSS.DicomViewer.Domain.ValueObjects; // Assuming value objects are here
-global using TheSSS.DicomViewer.Infrastructure.Persistence; // For DicomDbContext
-global using TheSSS.DicomViewer.Infrastructure.Services; // Assuming infra services are here
-global using TheSSS.DicomViewer.Common.Models; // Assuming common models are here
-global using TheSSS.DicomViewer.Common.Extensions; // Assuming common extensions are here
+// SUT Namespaces
+global using TheSSS.DicomViewer.Application.Services;
+global using TheSSS.DicomViewer.Infrastructure.Data;
+global using TheSSS.DicomViewer.Domain.Entities;
+global using TheSSS.DicomViewer.Common.Logging;
+global using TheSSS.DicomViewer.Presentation.ViewModels;
 
-global using TheSSS.DicomViewer.IntegrationTests.Fixtures;
-global using TheSSS.DicomViewer.IntegrationTests.Mocks;
-global using TheSSS.DicomViewer.IntegrationTests.Helpers;
+// Test Project Specific Namespaces (will be added as files are created)
+// global using TheSSS.DicomViewer.IntegrationTests.Fixtures;
+// global using TheSSS.DicomViewer.IntegrationTests.Mocks;
+// global using TheSSS.DicomViewer.IntegrationTests.Helpers;
